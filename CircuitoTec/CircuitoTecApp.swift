df50e5f0833351e2667ccf923d302446f1c3bc5c
carolinasntos <a01174992@tec.mx>
2024-09-10 11:26:26 -0600

    Initial Commit

---
+++ CircuitoTec/CircuitoTecApp.swift
@@ -0,0 +1,17 @@
+//
+//  CircuitoTecApp.swift
+//  CircuitoTec
+//
+//  Created by Carolina De los Santos Reséndiz on 10/09/24.
+//
+
+import SwiftUI
+
+@main
+struct CircuitoTecApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CircuitoTec/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  CircuitoTec
+//
+//  Created by Carolina De los Santos Reséndiz on 10/09/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
